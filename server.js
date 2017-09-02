bc885af7755cff5f8555a7c34ff31b5cf881525c
BHAVYA SHAH <31134529+bhavyasci@users.noreply.github.com>
2017-09-02 17:57:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,6 +11,102 @@ app.get('/', function (req, res) {
 
 
 
+
+
+
+var articleOne = {
+  title: 'ARTICLE One | BHAVYA SHAH',
+  heading: 'ARTICLE ONE',
+  date: '1 Sept 2017',
+  content: `
+             
+             <h4>Personal</h4>
+               <p>
+              This is some personal information   about me. 
+               </p>
+
+          <h4>Professional</h4>
+           <p>
+           This is a list of my work experiences: 
+            </p>
+         <ol>
+          <li> Company A: Worked as some very seriously</li>
+          <li> Company B: Worked without seriousness </li>
+         </ol>
+         
+        
+  `
+};
+
+
+
+function createTemplate(data){
+    
+    var title=data.title;
+    var heading=data.heading;
+    var date= data.date;
+    var content= data.content;
+
+     var template=
+    `
+    
+    
+    <html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    
+    <body>
+      <div class="container">  
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <hr/>
+        
+        <h3>${heading}</h3>
+        
+          <div>
+            ${date}
+          </div>
+        
+           <div>   
+          ${content}
+         
+        </div> 
+     </div>
+</body>
+</html>
+
+    
+    
+    
+    `
+;
+
+return(template);
+
+}
+
+
+
+
+
+
+
+
+app.get('/article-ONE', function (request ,response){
+   response.send(createTemplate(articleOne)); 
+});
+
+
+
+
 app.get('/article-one', function (request ,response){
    response.sendFile(path.join(__dirname, 'ui', 'article-one.html')); 
 });
